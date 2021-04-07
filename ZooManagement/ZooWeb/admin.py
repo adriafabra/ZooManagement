2897cd3099d0ea8f4176160c8eeb70f8a66f2e05
adriafabra <adria.fabra2508@gmail.com>
2021-04-07 19:43:36 +0200

    Completing models

--- ZooManagement/ZooWeb/admin.py
+++ ZooManagement/ZooWeb/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Animal, Worker, Sector, GroupOfVisitor
+from .models import Animal, Worker, Sector, GroupOfVisitor, GuidedTour
 
 admin.site.register(Animal)
 admin.site.register(Worker)
 admin.site.register(Sector)
 admin.site.register(GroupOfVisitor)
+admin.site.register(GuidedTour)
--- ZooManagement/ZooWeb/models.py
+++ ZooManagement/ZooWeb/models.py
@@ -4,38 +4,58 @@ from django.db import models
 
 
 class Animal(models.Model):
-
     TAXONOMY_LIST = (('M', 'Mammal'), ('R', 'Reptile'), ('B', 'Bird'), ('A', 'Amphibian'))
     ALIMENTATION_LIST = (('C', 'Carnivorous'), ('H', 'Herbivorous'), ('O', 'Omnivorous'),
                         ('F', 'Fruitarian'), ('I', 'Filter'), ('S', 'Scavenger'), 
                         ('I', 'Insectivorous'), ('P', 'Piscivorous'))
 
-    
-
-    species = models.CharField(max_length=20)
+    species = models.CharField(max_length=20, unique=True)
     classes = models.CharField(max_length=1, choices=TAXONOMY_LIST)
-    weight = models.IntegerField()
-    height = models.IntegerField()
+    weight_in_kilograms = models.IntegerField()
+    height_in_centimeters = models.IntegerField()
     life_expectancy = models.IntegerField()
     alimentation = models.CharField(max_length=2, choices=ALIMENTATION_LIST)
     sector = models.ForeignKey('Sector', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.species
    
 
 class Worker(models.Model):
-
-    NSS = models.IntegerField()
+    NSS = models.IntegerField(unique=True)
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
     age = models.IntegerField()
+    animals = models.ManyToManyField('Animal', blank=True)
+    sectors = models.ManyToManyField('Sector', blank=True)
+
+    def __str__(self):
+        return "%s %s" % (self.name, self.surname)
+    
 
 class Sector(models.Model):
-    SECTOR_LIST = (('M', 'Mountain'), ('F', 'Freshwater'), ('W', 'Wetlands'), ('FO', 'Forest'),
-                ('D', 'Desert'), ('S', 'Sea'), ('SA', 'Savannah'), ('J', 'Jungle'))
+    SECTOR_LIST = (('Mountain', 'Mountain'), ('Freshwater', 'Freshwater'), ('Wetlands', 'Wetlands'), ('Forest', 'Forest'),
+                ('Desert', 'Desert'), ('Sea', 'Sea'), ('Savannah', 'Savannah'), ('Jungle', 'Jungle'))
 
+    name = models.CharField(max_length=20, choices=SECTOR_LIST, unique=True)
+    area_in_square_meters = models.IntegerField(default=0)
 
-    name = models.CharField(max_length=2, choices=SECTOR_LIST)
+    def __str__(self):
+        return self.name
 
 class GroupOfVisitor(models.Model):
     representant = models.CharField(max_length=20)
     number_of_visitors = models.IntegerField()
+
+    def __str__(self):
+        return self.representant
+
+class GuidedTour(models.Model):
+    worker = models.ForeignKey('Worker', on_delete=models.CASCADE)
+    group_of_visitor = models.ForeignKey('GroupOfVisitor', on_delete=models.CASCADE)
+    sector = models.ManyToManyField('Sector')
+    date = models.DateField()
+
+    def __str__(self):
+        return "%s %s" % (self.group_of_visitor.representant, self.date)
 
