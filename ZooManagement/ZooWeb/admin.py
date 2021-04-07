f6f5c8c53bb5bb0b0002502caa3d29b12a535090
adriafabra <adria.fabra2508@gmail.com>
2021-04-07 18:14:52 +0200

    Editing models

--- ZooManagement/ZooWeb/admin.py
+++ ZooManagement/ZooWeb/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Animal, Worker, Sector, GroupOfVisitors
+from .models import Animal, Worker, Sector, GroupOfVisitor
 
 admin.site.register(Animal)
 admin.site.register(Worker)
 admin.site.register(Sector)
-admin.site.register(GroupOfVisitors)
+admin.site.register(GroupOfVisitor)
--- ZooManagement/ZooWeb/models.py
+++ ZooManagement/ZooWeb/models.py
@@ -4,36 +4,38 @@ from django.db import models
 
 
 class Animal(models.Model):
+
     TAXONOMY_LIST = (('M', 'Mammal'), ('R', 'Reptile'), ('B', 'Bird'), ('A', 'Amphibian'))
     ALIMENTATION_LIST = (('C', 'Carnivorous'), ('H', 'Herbivorous'), ('O', 'Omnivorous'),
                         ('F', 'Fruitarian'), ('I', 'Filter'), ('S', 'Scavenger'), 
                         ('I', 'Insectivorous'), ('P', 'Piscivorous'))
-    SECTOR_LIST = (('M', 'Mountain'), ('F', 'Freshwater'), ('W', 'Wetlands'), ('FO', 'Forest'),
-                ('D', 'Desert'), ('S', 'Sea'), ('SA', 'Savannah'), ('J', 'Jungle'))
 
     
 
-    species = models.CharField(max_length=100)
-    classes = models.CharField(max_length=100, choices=TAXONOMY_LIST)
+    species = models.CharField(max_length=20)
+    classes = models.CharField(max_length=1, choices=TAXONOMY_LIST)
     weight = models.IntegerField()
     height = models.IntegerField()
     life_expectancy = models.IntegerField()
-    alimentation = models.CharField(max_length=100, choices=ALIMENTATION_LIST)
-    sector = models.CharField(max_length=100, choices=SECTOR_LIST)
+    alimentation = models.CharField(max_length=2, choices=ALIMENTATION_LIST)
+    sector = models.ForeignKey('Sector', on_delete=models.CASCADE)
    
 
 class Worker(models.Model):
+
     NSS = models.IntegerField()
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    age = models.IntegerField()
 
 class Sector(models.Model):
     SECTOR_LIST = (('M', 'Mountain'), ('F', 'Freshwater'), ('W', 'Wetlands'), ('FO', 'Forest'),
                 ('D', 'Desert'), ('S', 'Sea'), ('SA', 'Savannah'), ('J', 'Jungle'))
 
 
-    name = models.CharField(max_length=100, choices=SECTOR_LIST)
+    name = models.CharField(max_length=2, choices=SECTOR_LIST)
 
-class GroupOfVisitors(models.Model):
-    representant = models.CharField(max_length=100)
+class GroupOfVisitor(models.Model):
+    representant = models.CharField(max_length=20)
     number_of_visitors = models.IntegerField()
 
