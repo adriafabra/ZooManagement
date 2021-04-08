5bc935220109007aa1a89bc9f0ecffb368c93788
adriafabra <adria.fabra2508@gmail.com>
2021-04-08 20:40:25 +0200

    Deployed on Heroku

--- ZooManagement/ZooManagement/settings.py
+++ ZooManagement/ZooManagement/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +125,5 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+django_heroku.settings(locals())
